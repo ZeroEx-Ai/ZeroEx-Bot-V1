3edd9b6a5d08f146667fbf7efe5c80196a4eb002
ZeroEx-Ai <jishudatta.b1972@gmail.com>
2025-02-05 11:28:16 +0600

    Update avt.js

--- Priyansh/commands/avt.js
+++ Priyansh/commands/avt.js
@@ -1,76 +1,84 @@
 module.exports.config = {
-  name: "avt",
+  name: "pp",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "get avt id by people using it",
+  description: "Get avatar by user ID or profile link",
   commandCategory: "Công cụ",
   cooldowns: 0
 };
 
-module.exports.run = async function({ api, event, args, Threads }) {
-const request = require("request");
-const fs = require("fs")
-const axios = require("axios")
-const threadSetting = (await Threads.getData(String(event.threadID))).data || {};
-const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
-const mn = this.config.name
-if (!args[0]) return api.sendMessage(`[⚜️]=== 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 𝗔𝗩𝗧 ===[⚜️]\n\n[⚜️]→ ${prefix}${mn} box is get avt your group\n\n[⚜️]→ ${prefix}${mn} id [id to get] <get the image of the uid of the people>\n\n[⚜️]→ ${prefix}${mn} link [link to get] <get the link of your people>\n\n[⚜️]→ ${prefix}${mn} user <the empty command is the get avatar of your user user>\n\n[⚜️]→ ${prefix}${mn} user [@mentions] <get avatar people tagged>`,event.threadID,event.messageID);
-  if (args[0] == "box") {
-           if(args[1]){ let threadInfo = await api.getThreadInfo(args[1]);
-           let imgg = threadInfo.imageSrc;
-       if(!imgg) api.sendMessage(`[⚜️]→ AVATAR your box ${threadInfo.threadName} here`,event.threadID,event.messageID);
-        else var callback = () => api.sendMessage({body:`[⚜️]→ Avata box ${threadInfo.threadName} here`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"), event.messageID); 
-      return request(encodeURI(`${threadInfo.imageSrc}`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-             }    
+module.exports.run = async function ({ api, event, args, Threads }) {
+  const request = require("request");
+  const fs = require("fs");
+  const axios = require("axios");
 
-            let threadInfo = await api.getThreadInfo(event.threadID);
-            let img = threadInfo.imageSrc;
-          if(!img) api.sendMessage(`[⚜️]→ AVATAR your box ${threadInfo.threadName} here`,event.threadID,event.messageID)
-          else  var callback = () => api.sendMessage({body:`[⚜️]→ AVATAR your box ${threadInfo.threadName} here`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"), event.messageID);   
-      return request(encodeURI(`${threadInfo.imageSrc}`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+  const threadSetting = (await Threads.getData(String(event.threadID))).data || {};
+  const prefix = threadSetting.hasOwnProperty("PREFIX") ? threadSetting.PREFIX : global.config.PREFIX;
+  const mn = this.config.name;
 
-}
-else if (args[0] == "id") {
-  try {
-  var id = args[1];
-  if (!id) return api.sendMessage(`[⚜️]→ Please enter uid to get avatar.`,event.threadID,event.messageID);
-   var callback = () => api.sendMessage({attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID);   
-   return request(encodeURI(`https://graph.facebook.com/${id}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
- }
- catch (e) {
-  api.sendMessage(`[⚜️]→ Can't get photo user.`,event.threadID,event.messageID);
- }
-}
-else if (args[0] == "link") {
-var link = args[1];
-if (!link) return api.sendMessage(`[⚜️]→ Please enter link to get avatar.`,event.threadID,event.messageID);
-var tool = require("fb-tools");
-try {
-var id = await tool.findUid(args[1] || event.messageReply.body);
-var callback = () => api.sendMessage({attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID);   
-return request(encodeURI(`https://graph.facebook.com/${id}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-}
-catch(e){
-    api.sendMessage("[⚜️]→ User does not exist.",event.threadID,event.messageID)
-}
-}
-else if(args[0] == "user") {
-  if (!args[1]) {
-    var id = event.senderID;
-    var callback = () => api.sendMessage({attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID);   
-    return request(encodeURI(`https://graph.facebook.com/${id}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+  if (!args[0]) {
+    return api.sendMessage(
+      `╭───𝗣𝗿𝗼𝗳𝗶𝗹𝗲 𝗣𝗶𝗰───◆\n` +
+        `⋄Get Group Photo: ${prefix}${mn} box\n` +
+        `⋄Get Profile Photo by user ID: ${prefix}${mn} id [user id]\n` +
+        `⋄Get Profile Photo by Facebook link: ${prefix}${mn} link [fb link]\n` +
+        `⋄Get your own Profile Photo: ${prefix}${mn}\n` +
+        `⋄Get Profile Photo of mentioned user: ${prefix}${mn} user [@mention]\n╰──────────◆`,
+      event.threadID,
+      event.messageID
+    );
   }
-  else if (args.join().indexOf('@') !== -1) {
-    var mentions = Object.keys(event.mentions)
-    var callback = () => api.sendMessage({attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID);   
-    return request(encodeURI(`https://graph.facebook.com/${mentions}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+
+  const fetchAvatar = async (id) => {
+    const filePath = __dirname + "/cache/avatar.png";
+    const avatarUrl = `https://graph.facebook.com/${id}/picture?height=720&width=720`;
+
+    request(encodeURI(avatarUrl))
+      .pipe(fs.createWriteStream(filePath))
+      .on("close", () => {
+        api.sendMessage({ attachment: fs.createReadStream(filePath) }, event.threadID, () => fs.unlinkSync(filePath), event.messageID);
+      });
+  };
+
+  if (args[0] === "box") {
+    let threadInfo = await api.getThreadInfo(event.threadID);
+    let img = threadInfo.imageSrc;
+    if (!img) return api.sendMessage(`⋄No Photo found for this group.`, event.threadID, event.messageID);
+
+    request(encodeURI(img))
+      .pipe(fs.createWriteStream(__dirname + "/cache/avatar.png"))
+      .on("close", () => {
+        api.sendMessage({ attachment: fs.createReadStream(__dirname + "/cache/avatar.png") }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/avatar.png"), event.messageID);
+      });
+  } else if (args[0] === "id") {
+    let id = args[1];
+    if (!id) return api.sendMessage(`⋄Please provide a user ID.`, event.threadID, event.messageID);
+    await fetchAvatar(id);
+  } else if (args[0] === "link") {
+    let link = args[1];
+    if (!link) return api.sendMessage(`⋄Please provide a Facebook profile link.`, event.threadID, event.messageID);
+
+    try {
+      let res = await axios.get(`https://id.traodoisub.com/api.php?link=${encodeURIComponent(link)}`);
+      if (res.data.success) {
+        await fetchAvatar(res.data.id);
+      } else {
+        api.sendMessage(`⋄Could not retrieve UID from the provided link.`, event.threadID, event.messageID);
+      }
+    } catch (e) {
+      api.sendMessage(`⋄Error retrieving UID. Make sure the link is correct.`, event.threadID, event.messageID);
+    }
+  } else if (args[0] === "user") {
+    if (!args[1]) {
+      await fetchAvatar(event.senderID);
+    } else if (Object.keys(event.mentions).length > 0) {
+      let mentions = Object.keys(event.mentions);
+      await fetchAvatar(mentions[0]);
+    } else {
+      api.sendMessage(`⋄Invalid command usage. Use ${prefix}${mn} to see available options.`, event.threadID, event.messageID);
+    }
+  } else {
+    api.sendMessage(`⋄Invalid command usage. Use ${prefix}${mn} to see available options.`, event.threadID, event.messageID);
   }
-  else {
-    api.sendMessage(`[⚜️]→ To install Sai order. Enter ${prefix}${mn} to see your module's commands..`,event.threadID,event.messageID);
-  }
-}
-else {
-  api.sendMessage(`[⚜️]→ To install Sai order. Enter ${prefix}${mn} to see your module's commands..`,event.threadID,event.messageID);
-}
-}
+};
