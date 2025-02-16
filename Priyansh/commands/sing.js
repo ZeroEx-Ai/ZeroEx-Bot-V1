4ece321f033e98f2cc732d0439a8ff19e2faa754
ZeroEx-Ai <jishudatta.b1972@gmail.com>
2025-02-17 02:22:51 +0600

    Update sing.js

--- Priyansh/commands/sing.js
+++ Priyansh/commands/sing.js
@@ -1,5 +1,5 @@
 const fs = require('fs-extra');
-const ytdl = require('@distube/ytdl-core');
+const ytdl = require('ytdl-core');
 const Youtube = require('youtube-search-api');
 const axios = require('axios');
 const convertHMS = (value) => new Date(value * 1000).toISOString().slice(11, 19);
