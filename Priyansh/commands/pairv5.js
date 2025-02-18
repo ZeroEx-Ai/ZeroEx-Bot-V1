65f922b16ed7292255b4b64ff9120df36dc4c633
ZeroEx-Ai <jishudatta.b1972@gmail.com>
2025-02-19 00:14:50 +0600

    Update pairv5.js

--- Priyansh/commands/pairv5.js
+++ Priyansh/commands/pairv5.js
@@ -1,18 +1,18 @@
-const coindown = 0
 module.exports.config = {
-	name: "pairv5",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "Joshua Sy",
-	description: "pair family",
-	commandCategory: "Fun",
-	cooldowns: 5,
-	dependencies: {
+    name: "pairv5",
+    version: "1.0.1",
+    hasPermssion: 0,
+    credits: "Adi.0X",
+    description: "Pair family",
+    commandCategory: "Fun",
+    cooldowns: 5,
+    dependencies: {
         "axios": "",
         "fs-extra": ""
     }
 }
-module.exports.onLoad = async() => {
+
+module.exports.onLoad = async () => {
     const { resolve } = global.nodemodule["path"];
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
@@ -34,16 +34,24 @@ async function makeImage({ one, two, three }) {
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
     let avatarThree = __root + `/avt_${three}.png`;
+
     let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+    
     let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-  let getAvatarThree = (await axios.get(`https://graph.facebook.com/${three}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    
+    let getAvatarThree = (await axios.get(`https://graph.facebook.com/${three}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarThree, Buffer.from(getAvatarThree, 'utf-8'));  
+    
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-  let circleThree = await jimp.read(await circle(avatarThree));
-    pairing_img.composite(circleOne.resize(65, 65), 135, 260).composite(circleTwo.resize(65, 65), 230, 210).composite(circleThree.resize(60, 60), 193, 370);
+    let circleThree = await jimp.read(await circle(avatarThree));
+    
+    pairing_img
+        .composite(circleOne.resize(65, 65), 135, 260)
+        .composite(circleTwo.resize(65, 65), 230, 210)
+        .composite(circleThree.resize(60, 60), 193, 370);
     
     let raw = await pairing_img.getBufferAsync("image/png");
     
@@ -51,40 +59,48 @@ async function makeImage({ one, two, three }) {
     fs.unlinkSync(avatarOne);
     fs.unlinkSync(avatarTwo);
     fs.unlinkSync(avatarThree);
+    
     return pathImg;
 }
+
 async function circle(image) {
     const jimp = require("jimp");
     image = await jimp.read(image);
     image.circle();
     return await image.getBufferAsync("image/png");
 }
-module.exports.run = async function({ api, event, args, Users, Threads, Currencies }) {
-  const axios = require("axios");
+
+module.exports.run = async function({ api, event, args, Users, Threads }) {
     const fs = require("fs-extra");
-  const { threadID, messageID, senderID } = event;
-   let balance = (await Currencies.getData(senderID)).money;
-    if (balance <= coindown) return api.sendMessage('Nghèo mà đòi ghép đôi =))',threadID,messageID);
-     await Currencies.decreaseMoney(event.senderID, parseInt(coindown));
-   
+    const { threadID, messageID, senderID } = event;
     
-   
-       if (!args[0]) {
-         var tl = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
-        var tle = tl[Math.floor(Math.random() * tl.length)];
-        var info = await api.getUserInfo(event.senderID);
-    var nameSender = info[event.senderID].name;
+    var tl = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
+    var tle = tl[Math.floor(Math.random() * tl.length)];
+    
+    // Get sender's info
+    var info = await api.getUserInfo(senderID);
+    var nameSender = info[senderID].name;
     var arraytag = [];
-        arraytag.push({id: event.senderID, tag: nameSender});
- let loz = await api.getThreadInfo(event.threadID);
-        var emoji = loz.participantIDs;
-        var e = emoji[Math.floor(Math.random() * emoji.length)];
- let abc = await api.getThreadInfo(event.threadID);
-        var emoji123 = loz.participantIDs;
-        var r = emoji123[Math.floor(Math.random() * emoji123.length)];
-         var name1 = (await Users.getData(e)).name
-         var name2 = (await Users.getData(r)).name
-var one = senderID, two = e, three = r;
-    return makeImage({ one, two, three }).then(path => api.sendMessage({ body: ` ${nameSender} x ${name1} x ${name2}\n-${coindown} 💸`,mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
-    }
+    arraytag.push({ id: senderID, tag: nameSender });
+    
+    // Get thread info and select two random participants
+    let threadInfo = await api.getThreadInfo(threadID);
+    var participantIDs = threadInfo.participantIDs;
+    
+    // Randomly select two IDs from the thread
+    var randomID1 = participantIDs[Math.floor(Math.random() * participantIDs.length)];
+    var randomID2 = participantIDs[Math.floor(Math.random() * participantIDs.length)];
+    
+    var name1 = (await Users.getData(randomID1)).name;
+    var name2 = (await Users.getData(randomID2)).name;
+    
+    var one = senderID, two = randomID1, three = randomID2;
+    
+    return makeImage({ one, two, three }).then(path =>
+        api.sendMessage({
+            body: `Happy Family 👪 Pair\n${nameSender} x ${name1} x ${name2}\nFamily Pair odds: ${tle}`,
+            mentions: arraytag,
+            attachment: fs.createReadStream(path)
+        }, threadID, () => fs.unlinkSync(path), messageID)
+    );
 }
